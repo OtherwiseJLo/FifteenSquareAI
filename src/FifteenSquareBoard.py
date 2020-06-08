33836dd0fc0373a0f32f8ad7ef39567db9334983
OtherwiseJLo <otherwisejlo@gmail.com>
2020-06-08 12:45:01 -0400

    Created find_emtpy_square

--- src/FifteenSquareBoard.py
+++ src/FifteenSquareBoard.py
@@ -10,8 +10,8 @@ class Square:
         pass
 
     def __repr__(self):
-        return str(self.value).rjust(2, ' ')
-    
+        return str(self.value).rjust(2, " ")
+
     def __str__(self):
         return str(self.value)
 
@@ -19,22 +19,26 @@ class Square:
 class FifteenSquareBoard:
     def __init__(self):
         squares = [Square(i) for i in range(1, 16)]
-        squares.append(Square(' '))
+        squares.append(Square(" "))
         assert len(squares) == 16
-        self.board = np.array(squares).reshape([4,4])
-    
+        self.board = np.array(squares).reshape([4, 4])
+        self.empty_square = self.board[3][3]
+
     def _shuffle(self):
         pass
 
+    def find_emtpy_square(self):
+        return np.where(self.board == self.empty_square)
+
     def __repr__(self):
-        divider = '+---+---+---+---+'
+        divider = "+---+---+---+---+"
         result = []
         result.append(divider)
         for i in range(4):
-            result.append('| {} | {} | {} | {} |'.format(*self.board[i, :]))
+            result.append("| {} | {} | {} | {} |".format(*self.board[i, :]))
             result.append(divider)
-        return '\n'.join(result)
+        return "\n".join(result)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test = FifteenSquareBoard()
